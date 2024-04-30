6995baf8a982e4f14240dcef1943bf4cb5809d5f
iahmedelbayaa <elbayaaahmed845@gmail.com>
2024-04-30 19:32:45 +0300

    Docs server file

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT ?? 3000;
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
