e838939d8cab5e56720008eb262911ed627d18bb
Ahmed Elbayaa <elbayaaahmed845@gmail.com>
2024-05-06 17:49:59 +0300

    Fix category controller

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,3 +1,4 @@
+import ApiError from "../utils/handle-api/api.error";
 import { CategoryService } from "./category.service";
 import { Request, Response } from "express";
 
@@ -8,29 +9,49 @@ export class CategoryController {
         this.categoryService = new CategoryService();
     }
 
-    async create (req: Request , res: Response) {
-        const category = await this.categoryService.create(req.body);
-        res.json(category)
+    async create(req: Request, res: Response) {
+        try {
+            const category = await this.categoryService.create(req.body);
+            res.json(category)
+        } catch (error: any) {
+            throw new ApiError(error.message)
+        }
     }
 
-    async findAll (req: Request , res: Response) {
-        const categories = await this.categoryService.findAll();
-        res.json(categories)
+    async findAll(req: Request, res: Response) {
+        try {
+            const categories = await this.categoryService.findAll();
+            res.json(categories)
+        } catch (error: any) {
+            throw new ApiError(error.message)
+        }
     }
 
-    async findOne (req: Request , res: Response) {
-        const category = await this.categoryService.findOne(Number(req.params.id));
-        res.json(category)
+    async findOne(req: Request, res: Response) {
+        try {
+            const category = await this.categoryService.findOne(Number(req.params.id));
+            res.json(category)
+        } catch (error: any) {
+            throw new ApiError(error.message)
+        }
     }
 
-    async update (req: Request , res: Response) {
-        const category = await this.categoryService.update(Number(req.params.id), req.body);
-        res.json(category)
+    async update(req: Request, res: Response) {
+        try {
+            const category = await this.categoryService.update(Number(req.params.id), req.body);
+            res.json(category)
+        } catch (error: any) {
+            throw new ApiError(error.message)
+        }
     }
 
-    async remove (req: Request , res: Response) {
-        const category = await this.categoryService.remove(Number(req.params.id));
-        res.json(category)
+    async remove(req: Request, res: Response) {
+        try {
+            const category = await this.categoryService.remove(Number(req.params.id));
+            res.json(category)
+        } catch (error: any) {
+            throw new ApiError(error.message)
+        }
     }
 
 }
