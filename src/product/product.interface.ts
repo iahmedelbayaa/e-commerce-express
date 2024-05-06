ae5337bb97b733e73123327f5baccfa147eadc39
Ahmed Elbayaa <elbayaaahmed845@gmail.com>
2024-05-06 19:57:35 +0300

    Docs product Entity && interface

---
+++ src/product/product.interface.ts
@@ -0,0 +1,15 @@
+export interface IProduct {
+    id: number;
+    name: string;
+    price: number;
+    description: string;
+    image: string;
+    quantity: number;
+    category: string;
+    rating: number;
+    discount: number;
+    status: string;
+    created_at: string;
+    updated_at: string;
+    deleted_at: string;
+}
