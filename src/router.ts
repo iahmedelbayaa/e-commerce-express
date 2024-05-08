3e3fb4d760fbafe23c1607a185f21d1f4b85b8aa
Ahmed Elbayaa <elbayaaahmed845@gmail.com>
2024-05-08 20:36:26 +0300

    Docs product router

--- src/router.ts
+++ src/router.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import userRouter from "./user/user.router";
 import roleRouter from "./role/role.router";
+import productRouter from "./product/product.router";
 
 
 const router = Router();
 
 router.use('/user', userRouter);
 router.use('/role', roleRouter);
+router.use('product', productRouter);
 
 
 export default router;
